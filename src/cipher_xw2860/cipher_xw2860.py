586b139f0d77fd349cf8da7c50bc78d0911ec3d1
Xin Yi Wong <xy.ivywong@gmail.com>
2022-10-30 20:33:36 -0400

    added docstrings and sphinx documentation

--- src/cipher_xw2860/cipher_xw2860.py
+++ src/cipher_xw2860/cipher_xw2860.py
@@ -1,4 +1,25 @@
 def cipher(text, shift, encrypt=True):
+    """"
+    A function that encrypts text but replacing each letter by a letter that's a certain
+    number of positions down the alphabet.
+
+    Parameters
+    -----------
+    text : text that is going to be encrypted
+    shift : the number of positions down the alphabet to be used for encryption
+    encrypt : takes in boolean values, decides whether to encrypt text or not
+
+    Returns
+    --------
+    Newly encrypted text.
+
+    Examples
+    ---------
+    >>>> import pandas as pd
+    >>>> cipher('Test', 5)
+    'Yjxy'
+
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
