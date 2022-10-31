2902d63a622f98c4dbc7b96e4cff1f8fe73d6c69
Xin Yi Wong <xy.ivywong@gmail.com>
2022-10-30 23:23:41 -0400

    added test file and published to testpypi

--- tests/test_cipher_xw2860.py
+++ tests/test_cipher_xw2860.py
@@ -1,1 +1,12 @@
 from cipher_xw2860 import cipher_xw2860
+import pytest
+
+# Test function for single word
+
+def test_single_cipher():
+    example = 'Test'
+    expected = 'Yjxy'
+    actual = cipher_xw2860.cipher(example, 5)
+    assert actual == expected
+
+    
